655cee95bf8915946f30885e0d1a528d41ba16da
Matthew Harvey <software@matthewharvey.net>
2013-10-10 11:38:01 +0000

    Added copyright_config file, to provide configuration details to "copyright.tcl" script.
    Got rid of old one-liner copyright notices.
    
    Ran "copyright.tcl" to insert expanded copyright/disclaimer
    notice at the top of every .hpp and .cpp file in Sqloxx project.
    
    
    git-svn-id: svn+ssh://192.168.1.25/home/matthew/Repos/repo_01/sqloxx/trunk@2399 ac3cbfda-f6ac-4daa-b570-d46c5f00fc98

--- include/detail/sqlite_dbconn.hpp
+++ include/detail/sqlite_dbconn.hpp
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the Sqloxx project and is distributed under the
+ * terms of the license contained in the file LICENSE.txt distributed
+ * with this package.
+ * 
+ * Author: Matthew Harvey <matthew@matthewharvey.net>
+ *
+ * Copyright (c) 2012-2013, Matthew Harvey.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #ifndef GUARD_sqlite_dbconn_hpp_8577399267152603
 #define GUARD_sqlite_dbconn_hpp_8577399267152603
 
@@ -11,7 +29,6 @@
  * \author Matthew Harvey
  * \date 04 July 2012.
  *
- * Copyright (c) 2012, Matthew Harvey. All rights reserved.
  * 
  * @todo Make the SQLiteDBConn class provide for a means
  * to check whether the particular file being connected to is
